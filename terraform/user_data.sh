8d2910831da5c25b7db27cb13bf2d79d4fca1aa0
AlexaD09 <dayaproyectos12@gmail.com>
2025-12-14 02:42:29 -0500

    deployment pokeapi9

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 set -e
 
-# Actualizar sistema
 yum update -y
-
-# Instalar Docker
 yum install -y docker
-
-# Iniciar Docker
-service docker start
-chkconfig docker on
-
-# Permitir docker sin sudo
+systemctl start docker
+systemctl enable docker
 usermod -aG docker ec2-user
 
 curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) \
@@ -22,7 +15,6 @@ mkdir -p /home/ec2-user/app
 cd /home/ec2-user/app
 
 curl -s -o docker-compose.yml https://raw.githubusercontent.com/AlexaD09/Pokeapi/main/docker-compose.yml
-
 
 /usr/local/bin/docker-compose pull
 /usr/local/bin/docker-compose up -d
